e265e3307714cb6c5deefe85f71c1553af59d42a
SelimF1998 <mohamedselim.feroukhi@enicar.ucar.tn>
2023-03-06 22:37:54 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+
+</script>
+
+<template>
+  <div>
+    <Navbar />
+  </div>
+  
+</template>
+
+<script>
+export default {
+    name: "App",
+    components: {
+      Navbar,
+
+    }
+    
+}
+</script>
+
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template lang="">
+  <div>
+    <nav class=" text-yellow-700 font-bold text-2xl py-2">
+      <div class="container mx-auto flex justify-between items-center">
+        <div class="flex gap-6">
+            <h1 class="cursor-pointer">Work</h1>
+            <h1 class="cursor-pointer">About</h1>
+        </div>
+        <div class="flex gap-6">
+            <h1 class="cursor-pointer">Contact</h1>
+            <h1 class="cursor-pointer" >Resume</h1>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: "Navbar",
+  setup() {
+    return {};
+  },
+};
+</script>
+<style lang=""></style>
